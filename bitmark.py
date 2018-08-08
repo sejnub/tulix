d37702ed0a3631eca48f83674bb5fc6a479dd18c
Heiner Bunjes <heiner@bunjes.net>
2018-08-09 00:24:18 +0200

    now  all entries are in holder. Time to start output html

--- bitmark.py
+++ bitmark.py
@@ -8,11 +8,14 @@ import re
 ## constants ##
 ###############
 
-NOMATCH     = 'no match'
-
 CURRENT_DIR = '<to be implemented>'
 INPUT_FN    = 'src/kopie_von_t59_####_hunte37_via_raspi-docker-02.tlp'
 OUTPUT_FN   = 'out.html'
+
+OK          = 'ok'
+ERROR       = 'error'
+MATCH       = 'did_match'
+NOMATCH     = 'did_not_match'
 
 
 #############
@@ -25,29 +28,52 @@ OUTPUT_FN   = 'out.html'
 ###############
 
 class Holder:
-    locals  = []
-    remotes = []
+    tunnels_all            = []
+    tunnels_browser_local  = []
+    tunnels_browser_remote = []
+    
     matchlevel_1_outer             = 0
     matchlevel_2_inner             = 0
     matchlevel_3_inner_convertable = 0
     matchlevel_4_inner_field_sizes = 0
+
+    def str_tunnels_all(self):
+        ret_value = ''
+        for entry in self.tunnels_all:
+            ret_value += str(entry) + '\n'
+        return(ret_value)
 
     def write_html(self, out_file):
         out_str = pprint.pformat(self)
         out_file.write(out_str)
 
 
+class Entry():
+    f_s_listen_if   = 0            
+    f_s_listen_port = 0            
+    f_s_dest_host   = 0          
+    f_s_dest_port   = 0          
+    f_s_comment     = 0        
+    f_v_listen_if   = ''          
+    f_v_listen_port = ''            
+    f_v_dest_host   = ''          
+    f_v_dest_port   = ''          
+    f_v_comment     = ''        
+
+    def __str__(self):
+        return(self.f_v_listen_port + ' | ' + self.f_v_comment)
+
+
+
 def pf_match(match_object):
-
-    pprint.pprint(type(match_object) )
-
     try:
         match_info = ((
-            "########\n" +
-            "{pretty}\n" +
-            "########\n" +
-            "span_hex: ({start_hex}, {end_hex}), length_of_match: {length}\n" +
-            "match: '{group}'"
+            "\n"                      + 
+            "match_object: {pretty}"  +
+            "\n"                      +
+            "entry:        '{group}'" +
+            "\n"                      +
+            "span_hex:     ({start_hex}, {end_hex}), length_of_match: {length}\n" 
 
             '').format(
                 pretty    = pprint.pformat(match_object),
@@ -62,143 +88,18 @@ def pf_match(match_object):
     return(match_info)
 
 
-
-def match_level_4(size_1, field_1, size_2, field_2, size_3, field_3, size_4, field_4, size_5, field_5, holder):
-    
-    # f_s : field size
-    # f_v : field value
-    
-    f_s_listen_if   = size_1
-    f_v_listen_if   = field_1
-    f_s_listen_port = size_2
-    f_v_listen_port = field_2
-    f_s_dest_host   = size_3
-    f_v_dest_host   = field_3
-    f_s_dest_port   = size_4
-    f_v_dest_port   = field_4
-    f_s_comment     = size_5
-    f_v_comment     = field_5
+def match_level_0(input_fn, output_fn):
 
     try:
-        if (
-            f_s_listen_if   < 3 or
-            f_s_listen_port < 1 or
-            f_s_dest_host   < 3 or
-            f_s_dest_port   < 1
-        ): 
-            print('Unmatched level 4')
-            return('error')
-        else:
-            print('Matched level 4')
-            holder.matchlevel_4_inner_field_sizes += 1
-            # WIP: 
-            holder.locals.append(f_v_listen_port + '   ' + f_v_comment)
+        with open(input_fn, "rb") as in_file:
+            in_bytes = in_file.read()
     except:
-        return(NOMATCH)
-    
+        reason = 'L0: File read failed'
+        print( ERROR, reason)
+        return(ERROR, reason)
 
-
-
-def match_level_2_3(matched_group, holder):
-
-    # WIP:
-
-    p2 = re.compile(b'(.)([^\00]*)\00\00\00(.)([^\00]*)\00\00\00(.)([^\00]*)\00\00\00(.)([^\00]*)\00\00\00(.)([^\00]*)$')
-    m = p2.search(matched_group)
-    match_info = pf_match(m)
-
-    print('################ Inner match')
-    if match_info == NOMATCH:
-        print('No inner match')
-    else:
-        holder.matchlevel_2_inner += 1
-        try:
-            size_1  = int.from_bytes(m.group(1), 'little')
-            field_1 = m.group(2).decode("utf-8")
-            size_2  = int.from_bytes(m.group(3), 'little')
-            field_2 = m.group(4).decode("utf-8")
-            size_3  = int.from_bytes(m.group(5), 'little')
-            field_3 = m.group(6).decode("utf-8")
-            size_4  = int.from_bytes(m.group(7), 'little')
-            field_4 = m.group(8).decode("utf-8")
-            size_5  = int.from_bytes(m.group(9), 'little')
-            field_5 = m.group(10).decode("utf-8")
-
-
-            groups_str = "## s1:{size_1} ## f1:{field_1} ## s2:{size_2} ## f2:{field_2} ## s3:{size_3} ## f3:{field_3} ## s4:{size_4} ## f4:{field_4} ## s5:{size_5} ## f5:{field_5} ".format(
-                size_1  = size_1,
-                field_1 = field_1,
-                size_2  = size_2,
-                field_2 = field_2,
-                size_3  = size_3,
-                field_3 = field_3,
-                size_4  = size_4,
-                field_4 = field_4,
-                size_5  = size_5,
-                field_5 = field_5
-            )
-
-            holder.matchlevel_3_inner_convertable += 1
-
-            print ('p11')
-
-        except:
-            groups_str = "Fields are not convertible"
-            return(NOMATCH)
-
-        print(groups_str)
-
-        match_level_4(
-            size_1  = size_1,
-            field_1 = field_1,
-            size_2  = size_2,
-            field_2 = field_2,
-            size_3  = size_3,
-            field_3 = field_3,
-            size_4  = size_4,
-            field_4 = field_4,
-            size_5  = size_5,
-            field_5 = field_5,
-            holder  = holder
-        )
-
-
-    return(1)
-
-
-
-
-
-def match_level_1(in_bytes, holder):
-
-    #print('\n############################## match1')
-    #p1 = re.compile(b'\01\00\00\00[^\00]...')
-    #m = p1.search(in_bytes)
-    #print(pf_match(m))
-
-    print('\n############################## match')
-
-    print("Number of bytes to parse: {n}".format(n=len(in_bytes)))
-    #p2 = re.compile(b'\01\00\00\00(.*?)((\01\00\00\00)|(\00\00\00\00\00\00\00\00\00))')
-    p2 = re.compile(b'\01\00\00\00(.*?)\01\00\00\00')
-    m = p2.search(in_bytes)
-    match_info = pf_match(m)
-    print(match_info)
-
-    if match_info != NOMATCH:
-
-        holder.matchlevel_1_outer += 1
-        result = match_level_2_3(m.group(1), holder)
-
-
-        match_level_1(in_bytes[m.span()[1]-5:], holder)
-
-
-
-
-def process_whole_file(input_fn, output_fn):
-    with open(input_fn, "rb") as in_file:
-        in_bytes = in_file.read()
+    value = 'L0: File read worked'
+    print( OK, value)
 
     holder = Holder()
     match_level_1(in_bytes, holder)
@@ -209,16 +110,184 @@ def process_whole_file(input_fn, output_fn):
     print("matchlevel_2: " + str(holder.matchlevel_2_inner))
     print("matchlevel_3: " + str(holder.matchlevel_3_inner_convertable))
     print("matchlevel_4: " + str(holder.matchlevel_4_inner_field_sizes))
-    
     print()
     print('Found the following entries:')
-    pprint.pprint(holder.locals)
+    pprint.pprint(holder.str_tunnels_all())
 
-    with open(output_fn, "w") as out_file:
-        holder.write_html(out_file)
+    try:
+        with open(output_fn, "w") as out_file:
+            holder.write_html(out_file)
+    except:
+        reason = 'L0: File write failed'
+        print( ERROR, reason)
+        return(ERROR, reason)
+
+    value = 'L0: File write worked'
+    print( OK, value)
+
+    ret_value = None
+
+    value = 'L0: Everything worked'
+    print( OK, value)
+    return(OK, value)
 
 
+def match_level_1(in_bytes, holder):
 
+    print()
+    print('################ match_level_1')
+    print("Number of bytes to parse: {n}".format(n=len(in_bytes)))
+
+    #p2 = re.compile(b'\01\00\00\00(.*?)((\01\00\00\00)|(\00\00\00\00\00\00\00\00\00))')
+    #p2 = re.compile(b'\01\00\00\00(.*?)\01\00\00\00')
+    p2 = re.compile(b'\01\00\00\00(.*?)((\01\00\00\00)|(\00\00\00\00\00\00\00\00\00))')
+    m = p2.search(in_bytes)
+    match_info = pf_match(m)
+    print(match_info)
+
+    if match_info == NOMATCH:
+        reason = 'L1: No more entry found'
+        print( ERROR, reason)
+        return(ERROR, reason)
+
+    value = 'L1: An entry was found'
+    print( OK, value)
+
+    holder.matchlevel_1_outer += 1
+    match_level_2(m.group(1), holder)
+
+    match_level_1(in_bytes[m.span()[1]-5:], holder)
+
+    return(OK, value)
+
+
+def match_level_2(matched_group, holder):
+
+    p2 = re.compile(b'(.)([^\00]*)\00\00\00(.)([^\00]*)\00\00\00(.)([^\00]*)\00\00\00(.)([^\00]*)\00\00\00(.)([^\00]*)$')
+    m = p2.search(matched_group)
+    match_info = pf_match(m)
+
+    if match_info == NOMATCH:
+        reason = 'L2: Entry does not have 5 fields'
+        print( ERROR, reason)
+        return(ERROR, reason)
+
+    value = 'L2: Five fields were found'
+    print( OK, value)
+
+    holder.matchlevel_2_inner += 1
+
+    match_level_3(m = m, holder = holder)
+
+    return(OK, value)
+
+
+def match_level_3(m, holder):
+
+    try:
+        size_1  = int.from_bytes(m.group(1), 'little')
+        field_1 = m.group(2).decode("utf-8")
+        size_2  = int.from_bytes(m.group(3), 'little')
+        field_2 = m.group(4).decode("utf-8")
+        size_3  = int.from_bytes(m.group(5), 'little')
+        field_3 = m.group(6).decode("utf-8")
+        size_4  = int.from_bytes(m.group(7), 'little')
+        field_4 = m.group(8).decode("utf-8")
+        size_5  = int.from_bytes(m.group(9), 'little')
+        field_5 = m.group(10).decode("utf-8")
+    except:
+        reason = 'Fields are not convertible'
+        print( ERROR, reason)
+        return(ERROR, reason)
+
+    value = 'L3: All fields could be converted'
+    print( OK, value)
+
+    groups_str = "## s1:{size_1} ## f1:{field_1} ## s2:{size_2} ## f2:{field_2} ## s3:{size_3} ## f3:{field_3} ## s4:{size_4} ## f4:{field_4} ## s5:{size_5} ## f5:{field_5} ".format(
+        size_1  = size_1,
+        field_1 = field_1,
+        size_2  = size_2,
+        field_2 = field_2,
+        size_3  = size_3,
+        field_3 = field_3,
+        size_4  = size_4,
+        field_4 = field_4,
+        size_5  = size_5,
+        field_5 = field_5
+    )
+    print(groups_str)
+
+    holder.matchlevel_3_inner_convertable += 1
+
+    match_level_4(
+        size_1  = size_1,
+        field_1 = field_1,
+        size_2  = size_2,
+        field_2 = field_2,
+        size_3  = size_3,
+        field_3 = field_3,
+        size_4  = size_4,
+        field_4 = field_4,
+        size_5  = size_5,
+        field_5 = field_5,
+        holder  = holder
+    )
+
+    return(OK, value)
+
+
+def match_level_4(size_1, field_1, size_2, field_2, size_3, field_3, size_4, field_4, size_5, field_5, holder):
+
+    # f_s : field size
+    # f_v : field value
+
+    entry = Entry()
+
+    entry.f_s_listen_if   = size_1
+    entry.f_v_listen_if   = field_1
+    entry.f_s_listen_port = size_2
+    entry.f_v_listen_port = field_2
+    entry.f_s_dest_host   = size_3
+    entry.f_v_dest_host   = field_3
+    entry.f_s_dest_port   = size_4
+    entry.f_v_dest_port   = field_4
+    entry.f_s_comment     = size_5
+    entry.f_v_comment     = field_5
+
+    try:
+        if (
+            entry.f_s_listen_if   < 3 or
+            entry.f_s_listen_port < 1 or
+            entry.f_s_dest_host   < 3 or
+            entry.f_s_dest_port   < 1
+        ):
+            reason = 'L4: Field sizes are not plausible'
+            print( ERROR, reason)
+            return(ERROR, reason)
+        else:
+            holder.matchlevel_4_inner_field_sizes += 1
+            value = 'L4: All fields sizes are plausible'
+            print( OK, value)
+    except:
+        reason = 'L4: Exception was raised'
+        print(ERROR, reason)
+        return(ERROR, reason)
+
+    match_level_5(entry, holder)
+
+    return(OK, value)
+
+
+def match_level_5(entry, holder):
+    holder.tunnels_all.append(entry)
+    value = 'L5: Entry was added to holder.tunnels_all'
+    print( OK, value)
+
+    # WIP:
+
+    value = 'L5: Ended successfully'
+    print( OK, value)
+    return(OK, value)
 
 
 
@@ -228,4 +297,4 @@ def process_whole_file(input_fn, output_fn):
 
 if __name__ == '__main__':
     print('p2')
-    process_whole_file(INPUT_FN, OUTPUT_FN)
+    match_level_0(INPUT_FN, OUTPUT_FN)
